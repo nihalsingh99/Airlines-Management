40e4fb6153ad6c1fcd9aea573f3bb9db00e0d46f
nihalsingh99 <nihal.kumar.singh@gmail.com>
2020-12-25 16:35:24 +0530

    db

--- DB/air.sql
+++ DB/air.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 21, 2020 at 07:20 PM
+-- Generation Time: Dec 25, 2020 at 12:04 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.33
 
@@ -32,17 +32,10 @@ CREATE TABLE `booking_done` (
   `username` varchar(50) NOT NULL,
   `flight_name` varchar(25) NOT NULL,
   `flight_PNR` varchar(25) NOT NULL,
-  `Date` date NOT NULL,
+  `Date` date DEFAULT NULL,
   `Time` time NOT NULL,
   `Price` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `booking_done`
---
-
-INSERT INTO `booking_done` (`booking_id`, `username`, `flight_name`, `flight_PNR`, `Date`, `Time`, `Price`) VALUES
-(2, 'nihalsiingh', 'AirAsia', 'AA992', '2020-12-25', '03:15:00', 3500);
 
 -- --------------------------------------------------------
 
@@ -62,13 +55,11 @@ CREATE TABLE `flights` (
 --
 
 INSERT INTO `flights` (`flight_name`, `flight_PNR`, `flight_time`, `Price`) VALUES
-('Air India', 'AI453', '02:31:00.000000', 4000),
 ('Air Asia', 'AA324', '03:15:00.000000', 3000),
 ('Indigo', 'IG291', '02:45:30.000000', 3120),
 ('Vistara', 'VA732', '03:12:00.000000', 4200),
 ('GoAir', 'GA421', '03:30:00.000000', 3200),
 ('SpiceJet', 'SJ945', '02:42:00.000000', 3800),
-('AirAsia', 'AA992', '03:02:00.000000', 4800),
 ('Vistara', 'VA249', '03:22:00.000000', 4380),
 ('AirIndia', 'AI422', '03:55:00.000000', 4250);
 
@@ -154,7 +145,7 @@ ALTER TABLE `user_reg`
 -- AUTO_INCREMENT for table `booking_done`
 --
 ALTER TABLE `booking_done`
-  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `logs`
